bec474b3ac847c2b7742e2118ec3189c730c6fc8
MaxTrainingRep <mxgryn@gmail.com>
2023-07-11 18:19:29 +0300

    New project

---
+++ src/services/MarvelService.js
@@ -0,0 +1,21 @@
+class MarvelService {
+    _apiBase  = 'https://gateway.marvel.com:443/v1/public/'
+    _apiKey='apikey=7499ace1130ad67874171a966a4b1288'
+    getResource = async (url) => {
+        let res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?${this._apiKey}`);
+    }
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
+export default MarvelService;
