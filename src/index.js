9a775ea7a8b94fd53c36ec90d3603b9829644c84
MaxTrainingRep <mxgryn@gmail.com>
2023-07-12 19:19:27 +0300

    Services update

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/app/App';
 
-import MarvelService from '../src/services/MarvelService';
 
 import './style/style.scss';
 
-const marvelService = new MarvelService();
 
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
 
 const root = createRoot(document.getElementById('root'));
