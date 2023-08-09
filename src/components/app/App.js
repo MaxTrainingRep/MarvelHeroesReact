225917c8b2b213e9054f4a7bdbc58c0cf35076ff
MaxTrainingRep <mxgryn@gmail.com>
2023-08-09 12:08:38 +0300

    Added React.Lazy for pages. Little optimization

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import { MainPage, ComicsPage, Page404, SingleComicPage } from "../pages";
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from "../spinner/Spinner";
 
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage =lazy(() => import('../pages/MainPage'));
+const ComicsPage =lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage =lazy(() => import('../pages/SingleComicPage'));
 
 const App = () => {
 
@@ -11,7 +16,8 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Routes>
+                    <Suspense fallback={<Spinner/>}>
+                          <Routes>
                         <Route path="/"
                             element={<MainPage />} />
                         <Route path="/comics"
@@ -20,7 +26,8 @@ const App = () => {
                             element={<SingleComicPage />} />
                         <Route path="*"
                             element={<Page404 />} />
-                   </Routes>
+                         </Routes>
+                    </Suspense>
                 </main>
             </div>
         </Router>
