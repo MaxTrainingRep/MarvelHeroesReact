408f9603835a02daf0f144eb25723345af3f3d53
MaxTrainingRep <mxgryn@gmail.com>
2023-09-03 16:59:20 +0300

    added react-helmet

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,11 +1,19 @@
+import { Helmet } from "react-helmet";
 import ComicsList from "../comicsList/ComicsList";
 import AppBanner from "../appBanner/AppBanner";
 
 const ComicsPage = () => {
     return (
-            <>
-                <AppBanner/>
-                <ComicsList/>
+        <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Page with comics"
+                />
+                <title>Comics Page</title>
+            </Helmet>
+            <AppBanner/>
+            <ComicsList/>
             </>
     )
 }
